0ae9a57f591cceeed84f18dd815c1e9fdf717499
nukemiko <north666dakota@gmail.com>
2022-10-06 20:00:15 +0800

    添加常用警告 LibTakiyashaWarning、CrypterCreateWarning、CrypterSaveWarning

---
+++ src/libtakiyasha/warns.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+
+class LibTakiyashaWarning(Warning):
+    pass
+
+
+class CrypterCreateWarning(LibTakiyashaWarning):
+    pass
+
+
+class CrypterSaveWarning(LibTakiyashaWarning):
+    pass
