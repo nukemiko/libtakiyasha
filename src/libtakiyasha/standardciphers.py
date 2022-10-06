196c9a122180ae61f6e00a1d5c9e617ffad64f3e
nukemiko <north666dakota@gmail.com>
2022-10-06 20:01:52 +0800

    添加了 StreamedAESWithModeECB 用于流式加解密非固定长度数据

---
+++ src/libtakiyasha/standardciphers.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from functools import partial
+from io import BytesIO
+from typing import Generator
+
+from pyaes import AESModeOfOperationECB
+from pyaes.util import append_PKCS7_padding, strip_PKCS7_padding
+
+from .common import BaseCipher
+
+
+class StreamedAESWithModeECB(BaseCipher):
+    @property
+    def blocksize(self) -> int | None:
+        return 16
+
+    def __init__(self, key, /) -> None:
+        super().__init__(key)
+
+        self._raw_cipher = AESModeOfOperationECB(key=self.key)
+
+    def yield_block(self, data: bytes) -> Generator[bytes, None, None]:
+        for blk in iter(partial(BytesIO(data).read, self.blocksize), b''):
+            yield blk
+
+    def encrypt(self, plaindata: bytes) -> bytes:
+        return b''.join(
+            self._raw_cipher.encrypt(b) for b in self.yield_block(append_PKCS7_padding(plaindata))
+        )
+
+    def decrypt(self, cipherdata: bytes) -> bytes:
+        return strip_PKCS7_padding(
+            b''.join(self._raw_cipher.decrypt(b) for b in self.yield_block(cipherdata))
+        )
