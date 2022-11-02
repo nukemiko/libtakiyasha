2f7d9c32272949f345b24acc2a9df3f4e2bf5f94
nukemiko <north666dakota@gmail.com>
2022-11-02 16:02:19 +0800

    重写了示例脚本

--- examples/dump-ncm-file-with-key.py
+++ examples/dump-ncm-file-with-key.py
@@ -1,27 +1,58 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from __future__ import annotations
 
 import argparse
+import os
+import re
 import shutil
 import sys
+from datetime import datetime
 from pathlib import Path
 
+from libtakiyasha.exceptions import LibTakiyashaException
 from libtakiyasha.ncm import NCM
 
-progname = Path(sys.argv[0]).name
-
 try:
-    from mutagen import flac, id3, mp3
+    from mutagen import flac, mp3, id3
 except ImportError:
-    print(f'{progname}：缺少依赖关系：mutagen', file=sys.stderr)
-    print(f"{progname}：请使用 'pip install -U mutagen' 安装后再次运行本程序。")
-    sys.exit(1)
+    mutagen_available = False
+else:
+    mutagen_available = True
+
 try:
     from tqdm import tqdm
 except ImportError:
-    print(f'{progname}：缺少依赖关系：tqdm', file=sys.stderr)
-    print(f"{progname}：请使用 'pip install -U tqdm' 安装后再次运行本程序。")
-    sys.exit(1)
+    tqdm_available = False
+else:
+    tqdm_available = True
+
+progname = Path(sys.argv[0]).name
+
+template_string_pattern = re.compile('{title}|{artist}|{album}|{time}')
+
+hexnumstr_array_sep_pattern = re.compile(', ?|,? ')
+hexnumstr_pattern = re.compile('^0x[0-9a-z]{,2}$', flags=re.IGNORECASE)
+
+if sys.platform.startswith('win'):
+    illegal_filename_chars_pattern = re.compile(r'[\x00-\x31~"#%&*:<>?/\\|]+')
+else:
+    illegal_filename_chars_pattern = re.compile(r'[\x00/]')
+
+
+def hexstring2bytes(hexstring: str, paramname: str) -> bytes:
+    try:
+        return bytes.fromhex(hexstring)
+    except ValueError:
+        hexnum_strings = hexnumstr_array_sep_pattern.split(hexstring)
+        hexnums: list[int] = []
+        for item in hexnum_strings:
+            try:
+                hexnums.append(int(item, base=16))
+            except ValueError:
+                print(f"错误：在参数 '{paramname}' 中发现无效的十六进制数字 '{item}'")
+                sys.exit(1)
+        return bytes(hexnums)
 
 
 def str_shorten(s, maxlen: int = 30, lr_maxkeeplen: int = 10) -> str:
@@ -34,182 +65,222 @@ def str_shorten(s, maxlen: int = 30, lr_maxkeeplen: int = 10) -> str:
     return string
 
 
-ap = argparse.ArgumentParser(
-    prog=progname,
-    formatter_class=argparse.RawTextHelpFormatter,
-    add_help=False
-)
-ap.add_argument('-t', '--filename-template',
-                metavar='TEMPLATE',
-                dest='target_filename_template',
-                default=None,
-                help='如果指定此选项，为输出文件的名称（不含扩展名）统一应用一个模板。\n'
-                     '如果解析模板字符串时出错，将会使用默认值“<去除扩展名的输入文件名>.<输出文件格式>”代替。\n'
-                     '可用模板关键字：\n'
-                     '  {title} - 标题\n'
-                     '  {artist} - 歌手（艺术家）\n'
-                     '  {album} - 专辑'
-                )
-ap.add_argument('-h', '--help',
-                action='help',
-                help='显示此帮助信息并退出'
-                )
-ap.add_argument('core_key',
-                metavar='<CORE KEY>',
-                help='解密音频数据所需的核心密钥，以十六进制形式表示。'
-                )
-ap.add_argument('sourcefiles_targetdir',
-                metavar='NCMFILES',
-                type=Path,
-                nargs='+',
-                help='所有输入文件的路径。最后一个路径如果指向目录，将会用于保存输出文件；\n'
-                     '否则，将输出文件保存到当前工作目录下。\n'
-                     '除了最后一个路径，其他路径必须指向文件。'
-                )
+ap = argparse.ArgumentParser(prog=progname,
+                             add_help=False,
+                             formatter_class=argparse.RawTextHelpFormatter,
+                             usage='%(prog)s [-h] [-t 模板| --template 模板] '
+                                   '(-k 核心密钥 | --core-key 核心密钥) '
+                                   'NCM 文件... '
+                                   '[输出目录]'
+                             )
+required_optargs = ap.add_argument_group('必需选项和参数')
+required_optargs.add_argument('-k', '--core-key',
+                              dest='core_key_str',
+                              metavar='核心密钥',
+                              required=True,
+                              help="解密文件所需的密钥，使用十六进制表示法。\n"
+                                   "可以接受以下形式：\n"
+                                   "  -k a1b1c4d5e1f4\n"
+                                   "  -k 0x11,0x45,0x14,0Xc1,0x9F,0x19,0xab\n"
+                                   "不区分大小写，包含的空格会被去除。"
+                              )
+required_optargs.add_argument('sources_or_target',
+                              metavar='NCM 文件... [输出目录]',
+                              nargs='+',
+                              type=Path,
+                              help='所有输入文件的路径。如果最后一个路径指向一个目录，那么它会被用作\n'
+                                   '所有输入文件的输出目录；否则，输出目录为当前目录。\n'
+                                   '除最后一个参数外，所有路径必须指向一个文件。'
+                              )
 
-optargs: dict[str, str | Path | list[Path]] = vars(ap.parse_intermixed_args())
+optional_optargs = ap.add_argument_group('可选选项和参数')
+optional_optargs.add_argument('-h', '--help',
+                              action='help',
+                              help='显示帮助信息并退出'
+                              )
+optional_optargs.add_argument('-t', '--template',
+                              dest='target_filename_template',
+                              metavar='模板',
+                              default='',
+                              help='以 <模板> 规定的格式设定输出文件的名称。\n'
+                                   '模板字符串中的可用字段：\n'
+                                   '  {title} - 标题\n'
+                                   '  {artist} - 歌手（艺术家）\n'
+                                   '  {album} - 专辑\n'
+                                   '  {time} - 文件生成的时间，使用 ISO 8601 表示法\n'
+                                   "例如，将歌曲标题作为输出文件名：-t '{title}'\n"
+                                   '如果未指定此选项，那么将会根据源文件名决定输出文件名。'
+                              )
+optional_optargs.add_argument('-n', '--no-tag',
+                              action='store_false',
+                              dest='with_tag',
+                              help='不要向输出文件中写入标签信息'
+                              )
 
-try:
-    core_key: bytes = bytes.fromhex(optargs['core_key'])
-except ValueError:
-    print(f"{progname}：错误：输入了无效的核心密钥（第一个位置参数）", file=sys.stderr)
-    print(f"{progname}：使用 '{progname} -h' 查看帮助信息。", file=sys.stderr)
-    sys.exit(1)
 
-sourcefiles: list[Path] = optargs['sourcefiles_targetdir']
+def main():
+    optargs = ap.parse_intermixed_args()
 
-targetdir: Path = sourcefiles.pop(-1)
-if not targetdir.is_dir():
-    sourcefiles.append(targetdir)
-    targetdir = Path.cwd()
-target_filename_template: str | None = optargs['target_filename_template']
+    core_key_str: str = optargs.core_key_str
+    core_key = hexstring2bytes(core_key_str, '-k/--core-key')
 
-total_tasks = len(sourcefiles)
-finished_tasks: list[tuple[Path, Path | str | Exception]] = []
-for taskno, source_path in enumerate(sourcefiles, start=1):
-    columns, lines = shutil.get_terminal_size()
-    print('=' * columns)
-    print(f"{progname}：正在进行：第 {taskno} 个任务，共 {total_tasks} 个")
+    sources_or_target: list[Path] = optargs.sources_or_target
+    targetdir = sources_or_target.pop(-1)
+    if not targetdir.is_dir():
+        sources_or_target.append(targetdir)
+        targetdir = Path.cwd()
 
-    source_filename = source_path.name
-    if not source_path.exists():
-        print(f"{progname}：跳过路径 '{source_path}'：路径不存在", file=sys.stderr)
-        finished_tasks.append((source_path, '路径不存在'))
-        print('=' * columns)
-        continue
-    elif source_path.is_dir():
-        print(f"{progname}：跳过路径 '{source_path}'：路径指向一个目录而不是文件", file=sys.stderr)
-        finished_tasks.append((source_path, '路径指向一个目录而不是文件'))
-        print('=' * columns)
-        continue
+    target_filename_template = optargs.target_filename_template
+    if target_filename_template != '' and not template_string_pattern.search(target_filename_template):
+        print(f"错误：文件名模板字符串 '{target_filename_template}' 不包含任何字段")
+        sys.exit(1)
+    target_filename_template = illegal_filename_chars_pattern.sub(
+        '%%', target_filename_template
+    )
 
-    try:
-        ncmfile = NCM.from_file(source_path, core_key=core_key)
-    except Exception as exc:
-        print(f"{progname}：跳过文件 '{str_shorten(source_filename)}'：解析文件时出现错误：{exc}", file=sys.stderr)
-        finished_tasks.append((source_path, exc))
-        print('=' * columns)
-        continue
-    else:
-        print(f"{progname}：成功打开文件 '{str_shorten(source_filename)}'")
+    with_tag: bool = optargs.with_tag
 
-    ncm_tag = ncmfile.ncm_tag
+    total = len(sources_or_target)
+    succeeds: list[tuple[Path, Path]] = []
+    fails: list[tuple[Path, str]] = []
 
-    target_filename_stem = source_path.stem
-    if target_filename_template is not None:
+    for current, sourcepath in enumerate(sources_or_target, start=1):
+        sourcepath_dirname = sourcepath.parent
+        sourcepath_filename = sourcepath.name
+        sourcepath_display = os.path.join(str_shorten(sourcepath_dirname), str_shorten(sourcepath_filename))
+
+        termcols, termls = shutil.get_terminal_size()
+        print('=' * termcols)
+        print(f"[{current}/{total}]输入文件：'{sourcepath_display}'")
+
+        errmsg = ''
+        if not sourcepath.exists():
+            errmsg = '路径不存在'
+        elif not sourcepath.is_file():
+            errmsg = '路径不是一个文件'
+        if errmsg:
+            print(f"跳过 '{sourcepath_display}'：{errmsg}")
+            fails.append((sourcepath, errmsg))
+            continue
+
         try:
-            title = '未知标题'
-            artist = '未知歌手'
-            album = '未知专辑'
-
-            if ncm_tag.musicName:
-                title = ncm_tag.musicName
-            artists_list = ncm_tag.artist
-            artists_names = [_[0] for _ in artists_list]
-            if len(artists_names) != 0:
-                artist = '、'.join(artists_names)
-            if ncm_tag.album:
-                album = ncm_tag.album
-
-            target_filename_stem = target_filename_template.format_map(
-                {
-                    'title' : title,
-                    'artist': artist,
-                    'album' : album
-                }
-            )
-        except Exception as exc:
-            print(f"{progname}：使用自定义文件名模板为 '{str_shorten(source_filename)}' 设置输出文件名时出错：{exc}")
-    target_filename = f'{target_filename_stem}.{ncm_tag.format}'
-    print(f"{progname}：设定 '{str_shorten(source_filename)}' 的输出文件名为 '{target_filename}'")
-
-    target_path = targetdir / target_filename
-
-    ncmfile_len: int = ncmfile.seek(0, 2)
-
-    print(f"{progname}：将 '{str_shorten(source_filename)}' 输出到 '{str_shorten(target_filename)}'...")
-    with open(target_path, mode='w+b') as targetfd:
-        with tqdm(total=ncmfile_len,
-                  desc=f"{progname}：{str_shorten(target_filename)}",
-                  unit='B',
-                  unit_scale=True,
-                  unit_divisor=1024
-                  ) as pbar:
+            ncmfile = NCM.from_file(sourcepath, core_key=core_key)
+            ncmfile_size = ncmfile.seek(0, 2)
             ncmfile.seek(0, 0)
-            for blk in ncmfile:
-                targetfd.write(blk)
-                pbar.update(len(blk))
-            finished_tasks.append((source_path, target_path))
-        print(f"{progname}：成功将 '{str_shorten(source_filename)}' 输出到 '{str_shorten(target_filename)}'！")
+        except LibTakiyashaException as exc:
+            errmsg = f'{type(exc).__name__}: {exc}'
+            print(f"跳过 '{sourcepath}'：{errmsg}")
+            fails.append((sourcepath, errmsg))
+            continue
 
-        try:
-            mutagen_style_dict = ncm_tag.to_mutagen_style_dict()
-            cover_data = ncmfile.cover_data
+        ncm_tag = ncmfile.ncm_tag
+        targetfile_format = ncm_tag.format.upper()
+        if not targetfile_format:
+            header_4bytes = ncmfile.read(4)
+            if header_4bytes.startswith(b'fLaC'):
+                targetfile_format = 'FLAC'
+            elif header_4bytes.startswith((b'ID3', b'\xff\xfb', b'\xff\xf3', b'\xff\xf2')):
+                targetfile_format = 'MP3'
+        print(f"输出文件格式：{targetfile_format.strip() if targetfile_format.strip() else '未知'}")
 
-            if ncm_tag.format.lower() == 'flac':
-                targetfd.seek(0, 0)
-                flactag = flac.FLAC(targetfd)
-                targetfd.seek(0, 0)
-                flactag.update(mutagen_style_dict)
-                if cover_data:
-                    picture = flac.Picture()
-                    picture.data = cover_data
-                    picture.type = 3
-                    if cover_data.startswith(b'\x89PNG'):
-                        picture.mime = 'image/png'
-                    elif cover_data.startswith(b'\xff\xd8\xff'):
-                        picture.mime = 'image/jpeg'
-                    flactag.add_picture(picture)
-                flactag.save(targetfd)
-            elif ncm_tag.format.lower() == 'mp3':
-                targetfd.seek(0, 0)
-                mp3tag = mp3.MP3(targetfd)
-                targetfd.seek(0, 0)
-                # mp3tag.update(mutagen_style_dict)
-                # mutagen.mp3.MP3 要求每个键的值都是 mutagen.id3.Frames 的子类的对象，
-                # 因此修改了修改标签的方式：
-                for key, value in mutagen_style_dict.items():
-                    id3frame_cls: id3.TIT2 | id3.TPE1 | id3.TALB = getattr(id3, key[:4])
-                    id3frame: id3.TextFrame | None = mp3tag.get(key)
-                    if id3frame is None:
-                        mp3tag[key] = id3frame_cls(text=value, desc='comment')
-                    elif not id3frame.text:
-                        id3frame.text = value
-                        mp3tag[key] = id3frame
-                if cover_data:
-                    picture = id3.APIC()
-                    picture.data = cover_data
-                    picture.type = 3
-                    if cover_data.startswith(b'\x89PNG'):
-                        picture.mime = 'image/png'
-                    elif cover_data.startswith(b'\xff\xd8\xff'):
-                        picture.mime = 'image/jpeg'
-                    mp3tag['APIC:'] = picture
-                mp3tag.save(targetfd)
-        except Exception as exc:
-            print(f"{progname}：设定输出文件 '{str_shorten(target_filename)}' 的标签和封面信息时出错：{exc}")
+        title = str(ncm_tag.musicName) if ncm_tag.musicName else None
+        if ncm_tag.artist:
+            artist_names_ids = list(ncm_tag.artist)
+            artist_names: list[str] = []
+            for item in artist_names_ids:
+                if len(item) < 1:
+                    continue
+                artist_names.append(str(item[0]))
+            artist = '、'.join(artist_names)
         else:
-            print(f"{progname}：成功为 '{str_shorten(target_filename)}' 设定了标签和封面信息")
+            artist = None
+        album = str(ncm_tag.album) if ncm_tag.album else None
 
-        print(f"{progname}：任务完成：'{str_shorten(source_filename)}' -> '{str_shorten(target_filename)}'")
-        print('=' * columns)
+        print(f"标题：{title if title else '无'}")
+        print(f"歌手：{artist if artist else '无'}")
+        print(f"专辑：{album if album else '无'}")
+
+        targetpath_filename = sourcepath.stem + f'.{targetfile_format.lower() if targetfile_format.strip() else "unknown"}'
+        if target_filename_template:
+            targetpath_filename = target_filename_template.format(
+                title=title,
+                artist=artist,
+                album=album,
+                time=datetime.now().isoformat(timespec='seconds')
+            ) + f'.{targetfile_format.lower() if targetfile_format.strip() else "unknown"}'
+        targetpath_filename_display = str_shorten(targetpath_filename)
+        targetpath = targetdir / targetpath_filename
+        print(f"输出文件名：{targetpath_filename}")
+        print(f"输出文件所在目录：{targetdir}")
+
+        if tqdm_available:
+            with tqdm(total=ncmfile_size,
+                      unit='B',
+                      unit_scale=True,
+                      desc=targetpath_filename_display
+                      ) as pbar:
+                with open(targetpath, 'wb') as targetfile:
+                    for blk in ncmfile:
+                        targetfile.write(blk)
+                        pbar.update(len(blk))
+        else:
+            with open(targetpath, 'wb') as targetfile:
+                for blk in ncmfile:
+                    targetfile.write(blk)
+            print('音频数据已取出')
+
+        if with_tag:
+            if mutagen_available:
+                try:
+                    cover_data = ncmfile.cover_data
+                    metadata = ncm_tag.to_mutagen_style_dict()
+                    if targetfile_format.upper() == 'FLAC':
+                        tag = flac.FLAC(targetpath)
+                        for key, value in metadata.items():
+                            tag[key] = value
+                        picture = flac.Picture()
+                        picture.data = cover_data
+                        picture.type = 3
+                        if cover_data.startswith(b'\x89PNG'):
+                            picture.mime = 'image/png'
+                        elif cover_data.startswith(b'\xff\xd8\xff'):
+                            picture.mime = 'image/jpeg'
+                        tag.add_picture(picture)
+                        tag.save(targetpath)
+                    elif targetfile_format.upper() == 'MP3':
+                        tag = mp3.MP3(targetpath)
+                        for key, value in metadata.items():
+                            id3frame_cls = getattr(id3, key[:4])
+                            id3frame = tag.get(key)
+                            if id3frame is None:
+                                tag[key] = id3frame_cls(text=value, desc='comment')
+                            elif id3frame.text:
+                                id3frame.text = value
+                                tag[key] = id3frame
+                        picture = id3.APIC()
+                        picture.data = cover_data
+                        picture.type = 3
+                        if cover_data.startswith(b'\x89PNG'):
+                            picture.mime = 'image/png'
+                        elif cover_data.startswith(b'\xff\xd8\xff'):
+                            picture.mime = 'image/jpeg'
+                        tag['APIC:'] = picture
+                        tag.save(targetpath)
+                    else:
+                        print('未嵌入标签信息，因为输出文件格式未知')
+                except Exception as exc:
+                    print(f'未能嵌入标签信息：{type(exc).__name__}: {exc}')
+                else:
+                    print('已嵌入标签信息')
+            else:
+                print("未嵌入标签信息，因为缺少依赖关系“mutagen”")
+
+        succeeds.append((sourcepath, targetpath))
+
+        if current == total:
+            termcols, termls = shutil.get_terminal_size()
+            print('=' * termcols)
+
+
+if __name__ == '__main__':
+    main()
