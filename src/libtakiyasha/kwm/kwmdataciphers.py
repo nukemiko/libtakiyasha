53b823cc7a2557cd26a43b685f33deb67f0dda78
nukemiko <north666dakota@gmail.com>
2022-12-30 15:18:35 +0800

    完成了对 KWM 相关功能的改造；增加了实验性的 KWM 文件导出功能

--- src/libtakiyasha/kwm/kwmdataciphers.py
+++ src/libtakiyasha/kwm/kwmdataciphers.py
@@ -8,53 +8,21 @@ from ..prototypes import KeyStreamBasedStreamCipherSkel
 from ..typedefs import BytesLike, IntegerLike
 from ..typeutils import tobytes, toint
 
-__all__ = ['Mask32']
+__all__ = ['Mask32', 'Mask32FromRecipe']
 
 
 class Mask32(KeyStreamBasedStreamCipherSkel):
-    @property
-    def core_key(self) -> bytes:
-        return self._core_key
+    def __init__(self, mask32: BytesLike, /) -> None:
+        self._mask32 = tobytes(mask32)
+        if len(self._mask32) != 32:
+            raise ValueError(f"invalid mask length: should be 32, got {len(self._mask32)}")
 
-    @property
-    def master_key(self) -> bytes:
-        return self._master_key
-
-    @property
-    def mask32(self) -> bytes:
-        return self._mask32
-
-    def __init__(self, core_key: BytesLike, master_key=BytesLike, /) -> None:
-        core_key = tobytes(core_key)
-        master_key = tobytes(master_key)
-
-        for varname, var, expectlen in ('core_key', core_key, 32), ('master_key', master_key, 8):
-            if len(var) != expectlen:
-                f"invalid length of argument '{varname}': should be {expectlen}, not {len(var)}"
-
-        self._core_key = core_key
-        self._master_key = master_key
-
-        mask_stage1 = str(int.from_bytes(master_key, 'little'))
-        if len(mask_stage1) >= 32:
-            mask_stage2 = mask_stage1[:32]
-        else:
-            mask_stage2_pad_len = 32 - len(mask_stage1)
-            mask_stage2_stage1_fullpad_count = (mask_stage2_pad_len // len(mask_stage1))
-            mask_stage2_stage1_fullpad_len = len(mask_stage1) * mask_stage2_stage1_fullpad_count
-            mask_stage2_remain_len = mask_stage2_pad_len - mask_stage2_stage1_fullpad_len
-
-            mask_stage2_composition = [mask_stage1]
-            for _ in range(mask_stage2_stage1_fullpad_count):
-                mask_stage2_composition.append(mask_stage1)
-            mask_stage2_composition.append(mask_stage1[:mask_stage2_remain_len])
-            mask_stage2 = ''.join(mask_stage2_composition).encode('utf-8')
-
-        mask_final = bytestrxor(mask_stage2, core_key)
-        self._mask32 = mask_final
+    def getkey(self, keyname: str = 'master') -> bytes | None:
+        if keyname == 'master':
+            return self._mask32
 
     @classmethod
-    def cls_keystream(cls, nbytes: IntegerLike, offset: IntegerLike, /, mask32: BytesLike) -> Generator[int, None, None]:
+    def cls_keystream(cls, mask32: BytesLike, nbytes: IntegerLike, offset: IntegerLike, /) -> Generator[int, None, None]:
         offset = toint(offset)
         nbytes = toint(nbytes)
         if offset < 0:
@@ -83,4 +51,45 @@ class Mask32(KeyStreamBasedStreamCipherSkel):
         yield from maskblk_data[:target_after_maskblk_area_len]
 
     def keystream(self, nbytes: IntegerLike, offset: IntegerLike, /) -> Generator[int, None, None]:
-        yield from self.cls_keystream(nbytes, offset, self._mask32)
+        yield from self.cls_keystream(self._mask32, nbytes, offset)
+
+
+class Mask32FromRecipe(Mask32):
+    def __init__(self, recipe: BytesLike, core_key: BytesLike, /) -> None:
+        recipe = tobytes(recipe)
+        core_key = tobytes(core_key)
+
+        for varname, var, expectlen in ('core_key', core_key, 32), ('recipe', recipe, 8):
+            if len(var) != expectlen:
+                f"invalid length of argument '{varname}': should be {expectlen}, not {len(var)}"
+
+        mask_recipe_unpacked = int.from_bytes(recipe, 'little')
+        mask_stage1 = str(mask_recipe_unpacked).encode('ascii')
+        if len(mask_stage1) >= 32:
+            mask_stage2 = mask_stage1[:32]
+        else:
+            mask_stage2_pad_len = 32 - len(mask_stage1)
+            mask_stage2_stage1_fullpad_count = (mask_stage2_pad_len // len(mask_stage1))
+            mask_stage2_stage1_fullpad_len = len(mask_stage1) * mask_stage2_stage1_fullpad_count
+            mask_stage2_remain_len = mask_stage2_pad_len - mask_stage2_stage1_fullpad_len
+
+            mask_stage2_composition = [mask_stage1]
+            for _ in range(mask_stage2_stage1_fullpad_count):
+                mask_stage2_composition.append(mask_stage1)
+            mask_stage2_composition.append(mask_stage1[:mask_stage2_remain_len])
+            mask_stage2 = b''.join(mask_stage2_composition)
+
+        mask_final = bytestrxor(mask_stage2, core_key)
+
+        self._source_recipe = recipe
+        self._core_key = core_key
+
+        super().__init__(mask_final)
+
+    def getkey(self, keyname: str = 'master') -> bytes | None:
+        if keyname == 'original':
+            return self._source_recipe
+        elif keyname == 'core':
+            return self._core_key
+        else:
+            return super().getkey(keyname)
