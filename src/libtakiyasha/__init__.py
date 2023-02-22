8088f6dbccd8ba4509a7df69e8c3f9d9ba82203f
nukemiko <north666dakota@gmail.com>
2023-02-23 00:47:30 +0800

    重大修改：不再导入常用子模块，而是直接导入常用函数和类

--- src/libtakiyasha/__init__.py
+++ src/libtakiyasha/__init__.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from . import kgmvpr, kwm, ncm, qmc
-from . import pkgmetadata
-
-__all__ = ['pkgmetadata', 'kgmvpr', 'kwm', 'ncm', 'qmc']
+from .exceptions import LibTakiyashaException
+from .kgmvpr import (
+    KGMorVPR,
+    KGMorVPRFileInfo,
+    probe_kgmvpr
+)
+from .kwm import (
+    KWM,
+    KWMFileInfo,
+    probe_kwm
+)
+from .ncm import (
+    NCM,
+    NCMFileInfo,
+    probe_ncm,
+    probeinfo_ncm
+)
+from .pkgmetadata import (
+    version,
+    version_info
+)
+from .qmc import (
+    QMCFileInfo,
+    QMCv1,
+    QMCv2,
+    probe_qmc,
+    probe_qmcv1,
+    probe_qmcv2,
+    probeinfo_qmc,
+    probeinfo_qmcv1,
+    probeinfo_qmcv2
+)
+from .warns import LibTakiyashaWarning
